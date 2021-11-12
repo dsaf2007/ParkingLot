e4cf8afd62708b6d1d4f2ce72b9e1c4013b19c91
gobi <ohhj1999@gmail.com>
2021-11-12 18:54:24 +0900

    main to home

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,4 +1,4 @@
-﻿// homepage.dart
+﻿// home.dart
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
--- lib/pages/main.dart
+++ lib/pages/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'search.dart';
+import 'home.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: SearchPage(title: '',),
+      home: HomePage()
     );
   }
 }
