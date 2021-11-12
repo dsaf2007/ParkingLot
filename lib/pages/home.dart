ad198d6364fe371ce8cf2a3aee72424af4073a46
Kim Kyungmin <qaws2051@gmail.com>
2021-11-12 21:19:36 +0900

    fix app name for google api

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -17,36 +17,29 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   // google map
   Completer<GoogleMapController> _controller = Completer();
-  static final CameraPosition _kGooglePlex = CameraPosition(
-    target: LatLng(37.42796133580664, -122.085749655962),
-    zoom: 14.4746,
-  );
-  static final CameraPosition _kLake = CameraPosition(
-      bearing: 192.8334901395799,
-      target: LatLng(37.43296265331129, -122.08832357078792),
-      tilt: 59.440717697143555,
-      zoom: 19.151926040649414);
+  // static final CameraPosition _kGooglePlex = CameraPosition(
+  //   target: LatLng(35, 134),
+  //   zoom: 15,
+  // );
+  static final CameraPosition _kLake =
+      CameraPosition(target: LatLng(35, 135), zoom: 15);
 
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      body: GoogleMap(
-        mapType: MapType.hybrid,
-        initialCameraPosition: _kGooglePlex,
-        onMapCreated: (GoogleMapController controller) {
-          _controller.complete(controller);
-        },
-      ),
-      floatingActionButton: FloatingActionButton.extended(
-        onPressed: _goToTheLake,
-        label: Text('To the lake!'),
-        icon: Icon(Icons.directions_boat),
+      bottomNavigationBar: NaviBarButtons(MediaQuery.of(context).size, context),
+      body: Container(
+        height: MediaQuery.of(context).size.height * 0.7,
+        width: MediaQuery.of(context).size.width * 0.7,
+        child: GoogleMap(
+          mapType: MapType.hybrid,
+          initialCameraPosition:
+              CameraPosition(target: LatLng(30, 60), zoom: 15),
+          onMapCreated: (GoogleMapController controller) {
+            _controller.complete(controller);
+          },
+        ),
       ),
     );
-  }
-
-  Future<void> _goToTheLake() async {
-    final GoogleMapController controller = await _controller.future;
-    controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
   }
 }
