e483282904d58f43e1dc2e309567ef604bd251a1
박소현 <shpark5938@naver.com>
2021-11-15 21:43:17 +0900

    ADD checkReservation, FIX page connections

---
+++ lib/pages/favorites.dart
@@ -0,0 +1,19 @@
+﻿import 'package:flutter/material.dart';
+import 'package:parkinglot/widget/navigationBar.dart';
+
+class Favorites extends StatefulWidget {
+  const Favorites({Key? key}) : super(key: key);
+
+  @override
+  _FavoritesState createState() => _FavoritesState();
+}
+
+class _FavoritesState extends State<Favorites> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: NaviBarButtons(MediaQuery.of(context).size, context),
+      body: Text("Favorites Page"),
+    );
+  }
+}
---
+++ lib/widget/navigationBar.dart
@@ -0,0 +1,104 @@
+﻿import 'package:flutter/material.dart';
+import 'package:parkinglot/pages/home.dart';
+import 'package:parkinglot/pages/favorites.dart';
+import 'package:parkinglot/pages/reservations.dart';
+import 'package:parkinglot/pages/search.dart';
+import 'package:parkinglot/pages/mypage.dart';
+import 'package:parkinglot/pages/CheckReservation.dart';
+
+Widget NaviBarButtons(Size size, BuildContext context) {
+  final double tabWidth = size.width * 0.2;
+  return Container(
+    height: 50,
+    child: Row(
+      children: <Widget>[
+        ButtonTheme(
+          minWidth: tabWidth,
+          height: size.height * 0.1,
+          child: RaisedButton(
+            color: Colors.white,
+            child: Icon(
+              Icons.home_outlined,
+              color: blue,
+            ),
+            onPressed: () {
+              Navigator.pop(context);
+              Navigator.push(
+                  context, MaterialPageRoute(builder: (context) => HomePage()));
+            },
+          ),
+        ),
+        ButtonTheme(
+          minWidth: tabWidth,
+          height: size.height * 0.1,
+          child: RaisedButton(
+            color: Colors.white,
+            child: Icon(
+              Icons.search,
+              color: blue,
+            ),
+            onPressed: () {
+              Navigator.pop(context);
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => SearchPage(
+                            title: "search",
+                          )));
+            },
+          ),
+        ),
+        ButtonTheme(
+          minWidth: tabWidth,
+          height: size.height * 0.1,
+          child: RaisedButton(
+            color: Colors.white,
+            child: Icon(
+              Icons.favorite_border,
+              color: blue,
+            ),
+            onPressed: () {
+              Navigator.pop(context);
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => Favorites()));
+            },
+          ),
+        ),
+        ButtonTheme(
+          minWidth: tabWidth,
+          height: size.height * 0.1,
+          child: RaisedButton(
+            color: Colors.white,
+            child: Icon(
+              Icons.receipt_outlined,
+              color: blue,
+            ),
+            onPressed: () {
+              Navigator.pop(context);
+              Navigator.push(context,
+                  MaterialPageRoute(
+                      builder: (context) => CheckReservation(
+                        title: "search",
+                      )));            },
+          ),
+        ),
+        ButtonTheme(
+          minWidth: tabWidth,
+          height: size.height * 0.1,
+          child: RaisedButton(
+            color: Colors.white,
+            child: Icon(
+              Icons.person_outline,
+              color: blue,
+            ),
+            onPressed: () {
+              Navigator.pop(context);
+              Navigator.push(
+                  context, MaterialPageRoute(builder: (context) => MyPage()));
+            },
+          ),
+        ),
+      ],
+    ),
+  );
+}
