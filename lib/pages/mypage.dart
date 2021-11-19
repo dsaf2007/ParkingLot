292139f6be2a2efa46fa72b8820aa513303780a0
unknown <dsaf2007@dongguk.edu>
2021-11-19 22:51:53 +0900

    DataBase in mypage

--- lib/pages/mypage.dart
+++ lib/pages/mypage.dart
@@ -16,163 +16,185 @@ class MyPage extends StatefulWidget {
 }
 
 class _MyPageState extends State<MyPage> {
+  dynamic temp = "";
+  String userName = "";
+  String userPhoneNumber = "010-****-1234"; // user.phoneNumber
   @override
   Widget build(BuildContext context) {
     //final String userName = "정동구"; // later.. user.name
-    dynamic temp;
-    FirebaseFirestore.instance
+
+    // print(temp);
+    /* FirebaseFirestore.instance
         .collection('User')
         .doc('leejaewon')
         .get()
         .then((DocumentSnapshot ds) {
-      temp = ds.get('Name');
+      // ignore: unnecessary_this
+      this.temp = ds.get('Name');
       print(temp + "data : ${ds.get('Name')}");
-    });
+    });*/
 
-    final String userName = temp;
-    final String userPhoneNumber = "010-****-1234"; // user.phoneNumber
     bool isAdmin = true;
     //comment for comit test
+    CollectionReference User = FirebaseFirestore.instance.collection('User');
 
-    return SafeArea(
-      child: Scaffold(
-        bottomNavigationBar:
-            NaviBarButtons(MediaQuery.of(context).size, context),
-        body: Container(
-          child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Container(
-                  padding:
-                      const EdgeInsets.symmetric(horizontal: 30, vertical: 20),
-                  child: Column(
+    return FutureBuilder<DocumentSnapshot>(
+      future: User.doc('leejaewon').get(),
+      builder:
+          (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot) {
+        if (snapshot.hasError) {
+          return Text("Sth Wrong");
+        }
+        if (snapshot.connectionState == ConnectionState.done) {
+          Map<String, dynamic> data =
+              snapshot.data!.data() as Map<String, dynamic>;
+          this.userName = data['Name'];
+          this.userPhoneNumber = data['User_PhoneNum'];
+
+          return SafeArea(
+            child: Scaffold(
+              bottomNavigationBar:
+                  NaviBarButtons(MediaQuery.of(context).size, context),
+              body: Container(
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: <Widget>[
-                      // SizedBox(
-                      //   height: 50,
-                      // ),
-                      Row(
-                        crossAxisAlignment: CrossAxisAlignment.end,
-                        children: <Widget>[
-                          Text(temp,
-                              style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                fontSize: 32,
-                              )),
-                          SizedBox(
-                            width: 10,
-                          ),
-                          isAdmin
-                              ? Text('관리자',
-                                  style: TextStyle(
-                                    fontWeight: FontWeight.bold,
-                                    fontSize: 16,
-                                  ))
-                              : Text(""),
-                        ],
-                      ),
-                      SizedBox(
-                        height: 10,
-                      ),
-                      Text(userPhoneNumber, style: TextStyle(fontSize: 18)),
-                      // SizedBox(
-                      //   height: 30,
-                      // )
-                    ],
-                  ),
-                ),
-                Divider(
-                  thickness: 5.0,
-                  color: lightGrey,
-                ),
-                Container(
-                  padding: const EdgeInsets.only(top: 5, left: 10),
-                  height: MediaQuery.of(context).size.height * 0.6,
-                  child: ListView(
-                      // itemExtent: 55, // height of list tile
-                      scrollDirection: Axis.vertical,
-                      children: <Widget>[
-                        ListTile(
-                          leading: Icon(Icons.sticky_note_2_outlined),
-                          title: Text("서비스 사용방법",
-                              style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                              )),
+                      Container(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 30, vertical: 20),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            // SizedBox(
+                            //   height: 50,
+                            // ),
+                            Row(
+                              crossAxisAlignment: CrossAxisAlignment.end,
+                              children: <Widget>[
+                                Text(userName,
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 32,
+                                    )),
+                                SizedBox(
+                                  width: 10,
+                                ),
+                                isAdmin
+                                    ? Text('관리자',
+                                        style: TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 16,
+                                        ))
+                                    : Text(""),
+                              ],
+                            ),
+                            SizedBox(
+                              height: 10,
+                            ),
+                            Text(userPhoneNumber,
+                                style: TextStyle(fontSize: 18)),
+                            // SizedBox(
+                            //   height: 30,
+                            // )
+                          ],
                         ),
-                        ListTile(
-                          leading: Icon(Icons.favorite_border_rounded),
-                          title: Text("즐겨찾기",
-                              style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                              )),
-                        ),
-                        ListTile(
-                          leading: Icon(Icons.notifications_none),
-                          title: Text("알림함",
-                              style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                              )),
-                          onTap: () {
-                            /* 알림함 페이지로 이동
+                      ),
+                      Divider(
+                        thickness: 5.0,
+                        color: lightGrey,
+                      ),
+                      Container(
+                        padding: const EdgeInsets.only(top: 5, left: 10),
+                        height: MediaQuery.of(context).size.height * 0.6,
+                        child: ListView(
+                            // itemExtent: 55, // height of list tile
+                            scrollDirection: Axis.vertical,
+                            children: <Widget>[
+                              ListTile(
+                                leading: Icon(Icons.sticky_note_2_outlined),
+                                title: Text("서비스 사용방법",
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                    )),
+                              ),
+                              ListTile(
+                                leading: Icon(Icons.favorite_border_rounded),
+                                title: Text("즐겨찾기",
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                    )),
+                              ),
+                              ListTile(
+                                leading: Icon(Icons.notifications_none),
+                                title: Text("알림함",
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                    )),
+                                onTap: () {
+                                  /* 알림함 페이지로 이동
                             Navigator.push(
                               context,
                               MaterialPageRoute(
                                   builder: (context) => AlarmList()),
                             );*/
-                          },
-                        ),
-                        ListTile(
-                          leading: Icon(Icons.person),
-                          title: Text("회원정보 수정",
+                                },
+                              ),
+                              ListTile(
+                                leading: Icon(Icons.person),
+                                title: Text("회원정보 수정",
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                    )),
+                              ),
+                              ListTile(
+                                leading: Icon(Icons.people),
+                                title: Text("팀 소개",
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                    )),
+                              ),
+                              ListTile(
+                                  leading: Icon(Icons.local_phone_outlined),
+                                  title: Text("고객센터",
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                      ))),
+                              ListTile(
+                                leading: Icon(Icons.help_outline),
+                                title: Text("FAQ",
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                    )),
+                              ),
+                            ]),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Text(
+                            "로그아웃",
+                          ),
+                          SizedBox(
+                            width: 20,
+                          ),
+                          Text("|",
                               style: TextStyle(
-                                fontWeight: FontWeight.bold,
+                                color: Colors.black54,
                               )),
-                        ),
-                        ListTile(
-                          leading: Icon(Icons.people),
-                          title: Text("팀 소개",
-                              style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                              )),
-                        ),
-                        ListTile(
-                            leading: Icon(Icons.local_phone_outlined),
-                            title: Text("고객센터",
-                                style: TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                ))),
-                        ListTile(
-                          leading: Icon(Icons.help_outline),
-                          title: Text("FAQ",
-                              style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                              )),
-                        ),
-                      ]),
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Text(
-                      "로그아웃",
-                    ),
-                    SizedBox(
-                      width: 20,
-                    ),
-                    Text("|",
-                        style: TextStyle(
-                          color: Colors.black54,
-                        )),
-                    SizedBox(
-                      width: 20,
-                    ),
-                    Text("회원탈퇴"),
-                  ],
-                ),
-              ]),
-        ),
-      ),
+                          SizedBox(
+                            width: 20,
+                          ),
+                          Text("회원탈퇴"),
+                        ],
+                      ),
+                    ]),
+              ),
+            ),
+          );
+        }
+        return Text("loading");
+      },
     );
   }
 }
