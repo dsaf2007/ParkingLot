c7430b51312338a90d93f9cbc6e7d34326caa3cd
Kim Kyungmin <qaws2051@gmail.com>
2021-11-28 15:09:03 +0900

    add dummy marker

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       // home: HomePage(),
-      home: ManageParkingLot(),
+      home: HomePage(),
     );
   }
 }
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,10 +1,12 @@
 ﻿// home.dart
 import 'dart:async';
 import 'package:flutter/material.dart';
-
+import 'package:parkinglot/pages/favorites.dart';
+import '../widget/navigation_bar.dart';
+import '../util/colors.dart';
+import 'datetime_selection.dart';
 // google map
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import '../widget/navigation_bar.dart';
 // map search
 // import 'package:address_search_field/address_search_field.dart';
 // import 'package:flutter_google_places/flutter_google_places.dart';
@@ -41,6 +43,179 @@ class _HomePageState extends State<HomePage> {
     //   isValidCurrentLocation = true;
     // });
 
+    // todo : maker builder, dynamic create
+    // List<Marker> mapBitmapsToMarkers(List<Uint8List> bitmaps) {itmaps.asMap().forEach((mid, bmp)}
+    List<Marker> markers = [
+      Marker(
+          markerId: MarkerId("testMarker1"),
+          position: LatLng(37.552, 127),
+          onTap: () {
+            showDialog(
+                context: context,
+                builder: (context) => AlertDialog(
+                      title: Text("Test"),
+                    ));
+          }),
+      Marker(
+        infoWindow: InfoWindow(
+          title: "행복주차장",
+          onTap: () {
+            showDialog(
+              context: context,
+              builder: (context) => AlertDialog(
+                contentPadding: EdgeInsets.symmetric(horizontal: 20),
+                content: Container(
+                  height: MediaQuery.of(context).size.height * 0.7,
+                  child: Column(children: <Widget>[
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        IconButton(
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                          icon: Icon(Icons.close),
+                        ),
+                      ],
+                    ),
+                    Image.asset(
+                      'lib/images/park.png',
+                      height: 150,
+                    ),
+                    Divider(
+                      height: 50,
+                      thickness: 2,
+                    ),
+                    Row(
+                      children: [
+                        Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text("주차장A",
+                                  style: TextStyle(
+                                      fontSize: 23,
+                                      color: blue,
+                                      fontWeight: FontWeight.bold)),
+                              SizedBox(height: 5),
+                              Text("주차장A주소"),
+                              Text("주차장A전화번호"),
+                              SizedBox(
+                                height: 5,
+                              ),
+                              Text('30분 500 원   |   총 180 면'),
+                            ]),
+                      ],
+                    ),
+                    SizedBox(height: 20),
+                    Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          TextButton(
+                            onPressed: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  // todo
+                                  builder: (context) => FavoritesPage(),
+                                ),
+                              );
+                            },
+                            style: TextButton.styleFrom(
+                                backgroundColor: lightGrey,
+                                minimumSize: Size(200, 30)),
+                            child: const Text('즐겨찾기 추가',
+                                style: TextStyle(color: Colors.black)),
+                          ),
+                          TextButton(
+                            onPressed: () {
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) =>
+                                          DateTimeSelection()));
+                            },
+                            style: TextButton.styleFrom(
+                                backgroundColor: blue,
+                                minimumSize: Size(200, 30)),
+                            child: const Text('예약하기',
+                                style: TextStyle(color: Colors.white)),
+                          ),
+                          SizedBox(height: 5),
+                        ])
+                  ]),
+                ),
+                // --- 이미지 넣기 ---
+              ),
+            );
+            // Navigator.push(
+            //   context,
+            //   MaterialPageRoute(
+            //     builder: (context) => Card(
+            //       child: ListTile(
+            //         onTap: () {},
+            //         subtitle: Column(children: [
+            //           Row(
+            //             children: [
+            //               Column(
+            //                   crossAxisAlignment: CrossAxisAlignment.start,
+            //                   children: [
+            //                     Text("주차장A",
+            //                         style: TextStyle(
+            //                             fontSize: 23,
+            //                             color: blue,
+            //                             fontWeight: FontWeight.bold)),
+            //                     SizedBox(height: 5),
+            //                     Text("주차장A주소"),
+            //                     Text("주차장A전화번호"),
+            //                     Text('30분 500 원   |   총 180 면'),
+            //                   ]),
+            //               SizedBox(width: 80),
+            //               Image.asset('lib/images/park.png',
+            //                   width: 100, height: 100),
+            //             ],
+            //           ),
+            //           SizedBox(height: 5),
+            //           Row(
+            //               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            //               children: [
+            //                 TextButton(
+            //                   onPressed: () {},
+            //                   style: TextButton.styleFrom(
+            //                       backgroundColor: lightGrey,
+            //                       minimumSize: Size(165, 20)),
+            //                   child: const Text('즐겨찾기 추가',
+            //                       style: TextStyle(color: Colors.black)),
+            //                 ),
+            //                 SizedBox(width: 10),
+            //                 TextButton(
+            //                   onPressed: () {
+            //                     Navigator.push(
+            //                         context,
+            //                         MaterialPageRoute(
+            //                             builder: (context) =>
+            //                                 DateTimeSelection()));
+            //                   },
+            //                   style: TextButton.styleFrom(
+            //                       backgroundColor: blue,
+            //                       minimumSize: Size(165, 20)),
+            //                   child: const Text('예약하기',
+            //                       style: TextStyle(color: Colors.white)),
+            //                 ),
+            //                 SizedBox(height: 5),
+            //               ])
+            //         ]),
+            //         // --- 이미지 넣기 ---
+            //       ),
+            //     ),
+            //   ),
+            // );
+          },
+        ),
+        markerId: MarkerId("testMarker2"),
+        position: LatLng(37.562, 127),
+      ),
+    ];
     return Scaffold(
       bottomNavigationBar: NaviBarButtons(MediaQuery.of(context).size, context),
       body: Center(
@@ -48,6 +223,7 @@ class _HomePageState extends State<HomePage> {
           height: MediaQuery.of(context).size.height * 0.7,
           width: MediaQuery.of(context).size.width * 0.9,
           child: GoogleMap(
+            markers: markers.toSet(),
             mapType: MapType.normal,
             initialCameraPosition: seoul,
             onMapCreated: (GoogleMapController controller) {
