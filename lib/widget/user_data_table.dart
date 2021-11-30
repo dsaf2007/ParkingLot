5f41ba6290b264711c07620ca31d027349e520f3
HeeJeongOh <ohhj1999@gmail.com>
2021-12-01 02:06:09 +0900

    check_reservation_admin edited 02:05

--- lib/widget/user_data_table.dart
+++ lib/widget/user_data_table.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:parkinglot/models/user_item.dart';
 import 'package:parkinglot/pages/check_reservation.dart';
 import '../util/colors.dart';
 
 Widget UserDataTableWidget(BuildContext context) {
-  CollectionReference users = FirebaseFirestore.instance.collection('User');
+  final Stream<QuerySnapshot> users = FirebaseFirestore.instance
+      .collection('User')
+      .snapshots(includeMetadataChanges: true);
 
   List<DataColumn> _getColumns = [
     DataColumn(
@@ -32,66 +32,54 @@ Widget UserDataTableWidget(BuildContext context) {
         '예약확인',
         textAlign: TextAlign.center,
       ),
-    )
+    ),
   ];
 
   List<DataRow> userList = [];
 
-  return FutureBuilder<QuerySnapshot>(
-      future: users.get(),
+  return StreamBuilder<QuerySnapshot>(
+      stream: users,
       builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
         if (snapshot.hasError) {
           return Text("Sth Wrong");
         }
-        if (snapshot.connectionState == ConnectionState.done) {
-          // Map<String, dynamic> data =
-          //     snapshot.data!.data() as Map<String, dynamic>;
-          // this.userName = data['Name'];
-          // this.userPhoneNumber = data['User_PhoneNum'];
-          int i = 1;
-          for (var doc in snapshot.data!.docs) {
-            //ParkingLotItem(this.image_path, this.name, this.address, this.telephone,
-            // this.minute, this.fee, this.total_space, this.favorite)
-            // UserItem(this.name, this.telephone, this.email)
-            List<DataCell> cellItem = [];
-            cellItem.add(DataCell(Text(i.toString())));
-            cellItem.add(DataCell(Text(doc["name"])));
-            cellItem.add(DataCell(Text(doc["user_telephone"])));
-            cellItem.add(DataCell(TextButton(
-              child: const Text("예약확인"),
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) =>
-                          CheckReservation(userId: doc["name"])),
-                );
-              },
-            )));
-
-            userList.add(DataRow(cells: cellItem));
-            i += 1;
-          }
-          return Container(
-              width: 330,
-              height: 400,
-              child: SingleChildScrollView(
-                  child: DataTable(
-                headingRowHeight: 40,
-                dataRowHeight: 40,
-                headingTextStyle:
-                    TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
-                headingRowColor: MaterialStateColor.resolveWith(
-                  (states) {
-                    return lightGrey;
-                  },
-                ),
-                dividerThickness: 2,
-                columnSpacing: 20.0,
-                columns: _getColumns,
-                rows: userList,
-              )));
+        int i = 1;
+        for (var doc in snapshot.data!.docs) {
+          List<DataCell> cellItem = [];
+          cellItem.add(DataCell(Text(i.toString())));
+          cellItem.add(DataCell(Text(doc["name"])));
+          cellItem.add(DataCell(Text(doc["user_telephone"])));
+          cellItem.add(DataCell(TextButton(
+            child: const Text("예약확인"),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) =>
+                        CheckReservation(userId: doc["name"])),
+              );
+            },
+          )));
+          userList.add(DataRow(cells: cellItem));
+          i += 1;
         }
-        return CircularProgressIndicator();
+        return Container(
+            width: 325,
+            height: 310,
+            child: SingleChildScrollView(
+                child: DataTable(
+                    headingRowHeight: 40,
+                    dataRowHeight: 40,
+                    headingTextStyle: TextStyle(
+                        color: Colors.black, fontWeight: FontWeight.bold),
+                    headingRowColor: MaterialStateColor.resolveWith(
+                      (states) {
+                        return lightGrey;
+                      },
+                    ),
+                    dividerThickness: 2,
+                    columnSpacing: 20.0,
+                    columns: _getColumns,
+                    rows: userList)));
       });
 }
